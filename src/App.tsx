7e9d3b1f9e185c940dae7dbede5975b434ada790
alux444 <aluux999@gmail.com>
2023-09-03 22:35:30 +1200

    fetchChampsInfo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import useDdragon from "./utils/useDdragon";
 
 function App() {
-    const { fetchChamps } = useDdragon();
+    const { fetchChamps, fetchChampionInfo } = useDdragon();
     fetchChamps();
+    fetchChampionInfo("Aatrox");
     return (
         <div>
             <p>hello</p>
--- src/utils/useDdragon.tsx
+++ src/utils/useDdragon.tsx
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 const useDdragon = () => {
-    const fetchChamps = async (version: string = "13.17.1") => {
-        console.log(version);
+    const fetchChamps = async () => {
+        let version: string = "13.17.1";
+
+        try {
+            const response = await axios.get(
+                `https://ddragon.leagueoflegends.com/api/versions.json`
+            );
+            const data = response.data[0];
+            version = data;
+            console.log(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
 
         try {
             const response = await axios.get(
@@ -15,7 +26,20 @@ const useDdragon = () => {
             console.error("Error fetching data:", error);
         }
     };
-    return { fetchChamps };
+
+    const fetchChampionInfo = async (champion: string) => {
+        try {
+            const response = await axios.get(
+                `http://ddragon.leagueoflegends.com/cdn/13.17.1/data/en_US/champion/${champion}.json`
+            );
+            const data = response.data;
+            console.log(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    return { fetchChamps, fetchChampionInfo };
 };
 
 export default useDdragon;
