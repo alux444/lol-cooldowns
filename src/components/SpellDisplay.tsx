7c7c708307e12bb5e78505170a0eaa9983ac8126
alux444 <aluux999@gmail.com>
2023-09-06 16:01:57 +1200

    change - display all levels

--- src/components/SpellDisplay.tsx
+++ src/components/SpellDisplay.tsx
@@ -1,37 +1,28 @@
 import { useState } from "react";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const SpellDisplay = ({ spell, cdr }: { spell: any; cdr: number }) => {
-    const [level, setLevel] = useState(1);
-
-    const increaseLevel = () => {
-        if (level + 1 <= spell.maxrank) {
-            setLevel(level + 1);
-        }
-    };
-
-    const decreaseLevel = () => {
-        if (level - 1 > 0) {
-            setLevel(level - 1);
-        }
-    };
+    const [selected, setSelected] = useState(0);
+    const cooldowns = [];
+    for (let i = 0; i < spell.maxrank; i++) {
+        cooldowns.push(
+            <button
+                onClick={() => setSelected(i)}
+                className={`flex gap-1 ${
+                    selected === i && "text-xl font-bold"
+                }`}
+            >
+                <p>{i + 1} - </p>
+                <p key={i}>{(spell.cooldown[i] * (1 - cdr)).toFixed(2)}s</p>
+            </button>
+        );
+    }
 
     return (
         <div className="flex items-center p-1 flex-col">
             <img
                 src={`https://ddragon.leagueoflegends.com/cdn/13.17.1/img/spell/${spell.image.full}`}
             />
-            <p>Level: {level}</p>
-            <div className="flex gap-1">
-                <button onClick={increaseLevel}>
-                    <KeyboardArrowUpIcon />
-                </button>
-                <button onClick={decreaseLevel}>
-                    <KeyboardArrowDownIcon />
-                </button>
-            </div>
-            <p>{(spell.cooldown[level - 1] * (1 - cdr)).toFixed(2)}s</p>
+            {cooldowns}
         </div>
     );
 };
